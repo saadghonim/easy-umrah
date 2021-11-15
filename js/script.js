5343a0442988eb1b29b9780399c3ddedc9019ffa
saadghonim <86053620+saadghonim@users.noreply.github.com>
2021-11-15 13:07:05 +0200

    edit css

--- js/script.js
+++ js/script.js
@@ -140,7 +140,8 @@ function initMap() {
 $( function() {
   $( "#datepicker" ).datepicker();
   $( "#datepicker_" ).datepicker();
-
+  $( "#datepickere" ).datepicker();
+  $( "#datepicker_e" ).datepicker();
 } );
 
 } 
